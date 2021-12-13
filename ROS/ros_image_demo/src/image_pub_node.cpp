9a5ff6173ccfbe3ae10deba5867a992478781874
jian <1479748954@qq.com>
2021-12-13 22:10:02 +0800

    小修改，暂时保存

--- ROS/ros_image_demo/src/image_pub_node.cpp
+++ ROS/ros_image_demo/src/image_pub_node.cpp
@@ -8,11 +8,13 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "image_publisher");
     ros::NodeHandle nh;
     image_transport::ImageTransport it(nh);
-    image_transport::Publisher pub = it.advertise("camera/image", 1);
+    // image_transport::Publisher pub = it.advertise("camera/image", 1);
+    image_transport::Publisher pub = it.advertise("/usb_cam/image_raw", 1);
 
     // NOTE: pay attention to the path of image file.
     // For CLion, set the working directory when debugging
-    cv::Mat image = cv::imread("./image0.png");
+    // cv::Mat image = cv::imread("./image0.png");
+    cv::Mat image = cv::imread("/home/jian/Documents/codes/vlc_detection/data/image2.png");
     sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
 
     ros::Rate loop_rate(5);
--- SLAM/cmake_learning/3_BOOK_cmake_practice/README.md
+++ SLAM/cmake_learning/3_BOOK_cmake_practice/README.md
@@ -2,6 +2,6 @@
 
 * 这本书很好。此目录正是这本书上的代码，略有修改。 
 
-* 主要掌握message, set和add_subdirectory这三个命令和PROJECT_SOURCE_DIR, CMAKE_CURRENT_SOURCE_DIR和PROJECT_BINARY_DIR这三个变量
+* 主要掌握message, set和add_subdirectory这三个命令和     PROJECT_SOURCE_DIR, CMAKE_CURRENT_SOURCE_DIR和PROJECT_BINARY_DIR这三个变量
 
 * 其余命令和变量，参考该书（已附上）
